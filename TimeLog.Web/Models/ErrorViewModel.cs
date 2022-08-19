bcc1477b5ced1d766b2cdd891b142c8ded9f9486
Ryan <eronkid@gmail.com>
2022-08-19 21:15:13 +0800

    Add project files.

---
+++ TimeLog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeLog.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
