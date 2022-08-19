36e27821397b0d8ad07d66326c2d91b1f1ab5621
Ryan <eronkid@gmail.com>
2022-08-19 22:25:45 +0800

    chore: Services & DAL

---
+++ TimeLog.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeLog.DAL.Data;
+using TimeLog.DAL.Interfaces;
+
+namespace TimeLog.DAL.Repositories
+{
+    public class BaseRepository : DescartesContext,  IBaseRepository
+    {
+    }
+}
