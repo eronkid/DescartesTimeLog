3e03687c99f88071b8665824a9c510ec67151939
Ryan <eronkid@gmail.com>
2022-08-19 23:36:19 +0800

    chore: Employee

--- TimeLog.DAL/Interfaces/IEmployeeRepository.cs
+++ TimeLog.DAL/Interfaces/IEmployeeRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeLog.DAL.Data.DescartesModels;
+using TimeLog.DAL.DtoModels;
 
 namespace TimeLog.DAL.Interfaces
 {
     public interface IEmployeeRepository : IBaseRepository
     {
+        void Create(Employee model);
+        Employee GetById(string id);
+        void Update(EmployeeDto modelDto);
+        void Delete(string id);
     }
 }
--- TimeLog.DAL/Repositories/EmployeeRepository.cs
+++ TimeLog.DAL/Repositories/EmployeeRepository.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeLog.DAL.Data.DescartesModels;
+using TimeLog.DAL.DtoModels;
 using TimeLog.DAL.Interfaces;
 
 namespace TimeLog.DAL.Repositories
 {
     public class EmployeeRepository : BaseRepository, IEmployeeRepository
     {
+        public void Create(Employee model)
+        {
+            model.DateCreated = DateTime.Now;
+            base.Employee.Add(model);
+            base.SaveChanges();
+        }
+
+        public Employee GetById(string id)
+        {
+            return base.Employee.Where(r => r.Id == id).FirstOrDefault();
+        }
+
+        public void Update(EmployeeDto modelDto)
+        {
+            var model = GetById(modelDto.Id);
+            model.FirstName = modelDto.FirstName;
+            model.MiddleName = modelDto.MiddleName;
+            model.LastName = modelDto.LastName;
+            model.DateModified = DateTime.Now;
+            
+            base.Employee.Attach(model);
+            base.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var model = new Employee() { Id = id };
+            base.Employee.Attach(model);
+            base.Employee.Remove(model);
+            base.SaveChanges();
+        }
     }
 }
