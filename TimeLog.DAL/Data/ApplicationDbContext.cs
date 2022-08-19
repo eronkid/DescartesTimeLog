18e61d8dd121f8d8de2a563d8fcac5807e880135
Ryan <eronkid@gmail.com>
2022-08-19 21:33:33 +0800

    chore: DAL

--- TimeLog.DAL/Data/ApplicationDbContext.cs
+++ TimeLog.DAL/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace TimeLog.Web.Data
+namespace TimeLog.DAL.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
